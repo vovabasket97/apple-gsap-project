3400800e91b6c504bae3cf79af7f9f4db944ff88
vovabasket97 <vovabasket97@gmail.com>
2024-07-12 13:39:27 +0200

    feat: highlights section

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -1,9 +1,42 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 import { FC } from 'react'
+
+import { rightImg, watchImg } from '../utils'
+
+import VideoCarousel from './VideoCarousel'
 
 interface HighlightsProps {}
 
 const Highlights: FC<HighlightsProps> = () => {
-  return <p>Highlights</p>
+  useGSAP(() => {
+    gsap.to('#title', { opacity: 1, y: 0 })
+    gsap.to('.link', { opacity: 1, y: 0, duration: 1, stagger: 0.25 })
+  }, [])
+
+  return (
+    <section id="highlights" className="common-padding size-full overflow-hidden bg-zinc">
+      <div className="screen-max-width">
+        <div className="mb-12 w-full items-end justify-between md:flex">
+          <h1 id="title" className="section-heading">
+            Get the highlights.
+          </h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="ml-2" />
+            </p>
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="right" className="ml-2" />
+            </p>
+          </div>
+        </div>
+
+        <VideoCarousel />
+      </div>
+    </section>
+  )
 }
 
 export default Highlights
